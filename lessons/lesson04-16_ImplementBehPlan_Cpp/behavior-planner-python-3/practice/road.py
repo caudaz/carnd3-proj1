7302ef11f459615dd49e371e4d3403a83f2214ee
root <root@fx8350.localdomain>
2017-10-17 08:43:00 -0500

    travel straight 50Hz X 0.5m = 25m/s

--- lessons/lesson04-16_ImplementBehPlan_Cpp/behavior-planner-python-3/practice/road.py
+++ lessons/lesson04-16_ImplementBehPlan_Cpp/behavior-planner-python-3/practice/road.py
@@ -22,7 +22,7 @@ class Road(object):
     for l in range(self.num_lanes):
       lane_speed = self.lane_speeds[l]
       vehicle_just_added = False
-      for s in range(start_s, start_s + self.update_width):
+      for s in range(int(start_s), int(start_s + self.update_width)):
         if vehicle_just_added:
           vehicle_just_added = False
           continue
